c4da96d5aa79252b7093b219770405d183b475f8
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-09-29 00:50:11 +0700

    allowed user to specify DE on linux

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-import path from 'path';
-import test from 'ava';
-import m from '.';
+import path from "path";
+import test from "ava";
+import m from ".";
 
-test('main', async t => {
+test("main", async t => {
 	const orignalImagePath = await m.get();
 
-	await m.set('fixture.jpg');
-	t.is(await m.get(), path.resolve('fixture.jpg'));
+	await m.set("fixture.jpg");
+	t.is(await m.get(), path.resolve("fixture.jpg"));
 
 	await m.set(orignalImagePath);
 });
